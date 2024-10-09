9bb0b14f5d2048153419430152254f89b5bb7c56
Lariiss <ferreira.silva.larissa0609@escola.pr.gov.br>
2024-10-09 09:28:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Como surgiu o metal?',
+    '  Surge como uma evolução natural do rock, misturando influências do blues rock, rock psicodélico e hard rock da época.   '
+)
+
+criaCartao(
+    'Nomes importantes',
+    'Quais foram as banda que fizeram historia no metal?',
+    'Black Sabbath, Led Zeppelin, Jimi Hendrix e Deep Purple foram algumas das bandas que marcaram o legado do metal. '
+)
+
+criaCartao(
+    'Habilidades',
+    'O que revolucionou o metal?',
+    'por ter sido um cara extremamente visionario e levou a arte de tocar guitarra a outros pataramares.'
+)
+
+criaCartao(
+    'Sepultura',
+    'O que é o Sepultura?',
+    'Sepultura foi uma banda de heavy metal brasileira extremamente influente no metal do mundo todo,a banda marcou muitas pessoas e seu legado ficará sempre marcado na historia da música.'
+)
